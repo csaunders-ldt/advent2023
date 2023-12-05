bd9b410307b2531e87ede7d2a97629ad18484365
Chris Saunders <chris@letsdothis.com>
2023-12-05 21:13:06 +0000

    feat: day5 (messy)

---
+++ day5/solve.ts
@@ -0,0 +1,59 @@
+import { chunk, max, min, sortBy } from 'lodash';
+import { solve } from '../utils/typescript';
+
+type Map = [dest: number, src: number, length: number];
+type Input = { seeds: number[]; maps: Map[][] };
+
+function getNumbers(line: string) {
+  return line.match(/\d+/g)?.map(Number) ?? [];
+}
+
+function parser(input: string) {
+  const [seedList, ...mapLines] = input.split(/\n\n.*\n/);
+  const seeds = getNumbers(seedList);
+  const maps = mapLines.map((l) => l.split('\n').map(getNumbers));
+  return { seeds, maps } as Input;
+}
+
+function applyMap(seed: number, maps: Map[]): number {
+  const map = maps.find(([, src, len]) => seed >= src && seed < src + len);
+  if (!map) return seed;
+  return seed + map[0] - map[1];
+}
+
+function part1({ seeds, maps }: Input) {
+  const results = seeds.map((seed) => maps.reduce(applyMap, seed));
+  return min(results);
+}
+
+function getCandidates(ranges: [number, number][], maps: Map[]) {
+  const candidates = ranges.map(([start, end]) => {
+    console.log({ start, end });
+    const overlaps = maps.filter(
+      ([, src, len]) => start <= src + len && src <= end,
+    );
+    if (!overlaps.length) return [[start, end]];
+    const sortedOverlaps = sortBy(overlaps, ([, src]) => src);
+
+    return sortedOverlaps.flatMap(([dest, src, len]) => {
+      const diff = dest - src;
+      const res = [[max([start, src]) + diff, min([src + len, end]) + diff]];
+      if (src > start) res.push([start, src - 1]);
+      if (end > start + len) res.push([end, start + len - 1]);
+
+      return res;
+    }) as [number, number][];
+  });
+  console.log(candidates.flat(1));
+  return candidates.flat(1);
+}
+
+function part2({ seeds, maps }: Input) {
+  const opts = maps.reduce(
+    (ranges, map) => getCandidates(ranges, map),
+    chunk(seeds, 2).map(([start, len]) => [start, start + len - 1]),
+  );
+  return min(opts.flat(2).filter((v) => v !== 0));
+}
+
+solve({ part1, test1: 35, part2, test2: 46, parser });
