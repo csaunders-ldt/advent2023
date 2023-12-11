0ec8a83ef7a384cc078f01597eb395fe39cc12af
Chris Saunders <cjasaunders@outlook.com>
2023-12-11 11:13:10 +0000

    feat: cleanup

--- day11/solve.ts
+++ day11/solve.ts
@@ -7,9 +7,9 @@ function parser(input: string): Input {
   return input.split('\n').map((l) => l.split(''));
 }
 
-function gaps(input: Input) {
+function gaps(input: Input, n = 1000000) {
   let gaps = 0;
-  return input.map((row) => (row.includes('#') ? gaps : gaps++));
+  return input.map((row) => (row.includes('#') ? gaps : (gaps += n)));
 }
 
 function manhattenDistance([[x1, y1], [x2, y2]]: number[][]) {
@@ -20,26 +20,20 @@ function pairs<T>(arr: T[]): T[][] {
   return arr.flatMap((v, i) => arr.slice(i + 1).map((w) => [v, w]));
 }
 
-function part1(input: Input) {
-  const gapsY = gaps(input);
-  const gapsX = gaps(zip(...input));
-  const points = input.flatMap((row, y) =>
-    row.flatMap((col, x) =>
-      col === '#' ? [[x + gapsX[x], y + gapsY[y]]] : [],
-    ),
+function points(input: Input, dx: number[], dy: number[]) {
+  return input.flatMap((row, y) =>
+    row.flatMap((col, x) => (col === '#' ? [[x + dx[x], y + dy[y]]] : [])),
   );
-  return sum(pairs(points).map(manhattenDistance));
+}
+
+function part1(input: Input) {
+  const toSum = points(input, gaps(zip(...input), 1), gaps(input, 1));
+  return sum(pairs(toSum).map(manhattenDistance));
 }
 
 function part2(input: Input) {
-  const gapsY = gaps(input);
-  const gapsX = gaps(zip(...input));
-  const points = input.flatMap((row, y) =>
-    row.flatMap((col, x) =>
-      col === '#' ? [[x + gapsX[x] * 1000000, y + gapsY[y] * 1000000]] : [],
-    ),
-  );
-  return sum(pairs(points).map(manhattenDistance));
+  const toSum = points(input, gaps(zip(...input)), gaps(input));
+  return sum(pairs(toSum).map(manhattenDistance));
 }
 
 solve({ part1, test1: 374, part2, test2: 82000292, parser });
