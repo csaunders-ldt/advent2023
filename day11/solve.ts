f066b8f96e38df8953318260924b160fb6ff5272
Chris Saunders <cjasaunders@outlook.com>
2023-12-11 11:41:19 +0000

    feat: be better

--- day11/solve.ts
+++ day11/solve.ts
@@ -1,4 +1,4 @@
-import { sum, zip } from 'lodash';
+import { zip } from 'lodash';
 import { solve } from '../utils/typescript';
 
 type Input = string[][];
@@ -7,32 +7,19 @@ function parser(input: string): Input {
   return input.split('\n').map((l) => l.split(''));
 }
 
-function gaps(input: Input, n = 1000000, gaps = 0) {
-  return input.map((row) => (row.includes('#') ? gaps : (gaps += n)));
-}
-
-function manhattenDistance([[x1, y1], [x2, y2]]: number[][]) {
-  return Math.abs(x1 - x2) + Math.abs(y1 - y2);
-}
-
-function pairs<T>(arr: T[]): T[][] {
-  return arr.flatMap((v, i) => arr.slice(i + 1).map((w) => [v, w]));
-}
-
-function points(input: Input, dx: number[], dy: number[]) {
-  return input.flatMap((row, y) =>
-    row.flatMap((col, x) => (col === '#' ? [[x + dx[x], y + dy[y]]] : [])),
-  );
+function sumDir([row, ...rest]: Input, expansion = 2, prev = 0, dist = 0) {
+  if (!row) return 0;
+  const numStars = row.filter((c) => c === '#').length;
+  dist += prev * (numStars === 0 ? expansion : 1);
+  return dist * numStars + sumDir(rest, expansion, prev + numStars, dist);
 }
 
 function part1(input: Input) {
-  const toSum = points(input, gaps(zip(...input), 1), gaps(input, 1));
-  return sum(pairs(toSum).map(manhattenDistance));
+  return sumDir(input) + sumDir(zip(...input));
 }
 
 function part2(input: Input) {
-  const toSum = points(input, gaps(zip(...input)), gaps(input));
-  return sum(pairs(toSum).map(manhattenDistance));
+  return sumDir(input, 1000000) + sumDir(zip(...input), 1000000);
 }
 
-solve({ part1, test1: 374, part2, test2: 82000292, parser });
+solve({ part1, test1: 374, part2, test2: 82000210, parser });
