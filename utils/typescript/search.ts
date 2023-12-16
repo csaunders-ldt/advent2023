cc57c0e5d139db2ecf9fada561bd6dbe6f471ab4
Chris Saunders <cjasaunders@outlook.com>
2023-12-16 15:31:31 +0000

    feat: example search algorithms

---
+++ utils/typescript/search.ts
@@ -0,0 +1,29 @@
+// Capture a set (e.g. `partialRight(pruneSeen, new Set())` and pass it to the below)
+export function pruneSeen<T>(set: T[], seen: Set<string>) {
+  return set.filter(
+    (t) => !(seen.has(JSON.stringify(t)) || seen.add(JSON.stringify(t))),
+  );
+}
+
+export function bfs<T>(
+  set: T[],
+  next: (t: T) => T[],
+  isDone: (t: T[]) => boolean = (set) => set.length === 0,
+  prune: (set: T[]) => T[] = (set) => set,
+) {
+  while (!isDone(set)) {
+    set = prune(set.flatMap(next));
+  }
+  return set;
+}
+
+export function dfs<T>(
+  set: T[],
+  next: (t: T) => T[],
+  isDone: (t: T[]) => boolean = (set) => set.length === 0,
+) {
+  while (!isDone(set)) {
+    set.unshift(...next(set.shift()));
+  }
+  return set;
+}
