7cf0a4026eea00e19895aafe580a10f4619869ed
Chris Saunders <cjasaunders@outlook.com>
2023-12-18 14:09:16 +0000

    fix: run latest day always

--- setup/advent.ts
+++ setup/advent.ts
@@ -1,2 +1,9 @@
-const dayToRun = process.argv[2] ?? new Date().getDate();
-require(`../day${dayToRun}/solve`);
+import { readdirSync } from 'fs';
+import { sortBy } from 'lodash';
+import { join } from 'path';
+
+const days = readdirSync(join(__dirname, '..')).filter((f) =>
+  f.startsWith('day'),
+);
+const sortedDays = sortBy(days, (d) => parseInt(d.slice(3)));
+require(`../${sortedDays.at(-1)}/solve`);
