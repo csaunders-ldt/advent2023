d2349685d043d5a4a983095868a8ff9eed9283a3
Chris Saunders <cjasaunders@outlook.com>
2023-12-22 22:32:57 +0000

    refactor[day22]: delete more dead code

--- day22/solve.ts
+++ day22/solve.ts
@@ -15,7 +15,7 @@ function parseLine(line: string): Coordinate {
 }
 
 function parser(input: string): Input {
-  return input.split('\n').map((l, i) => {
+  return input.split('\n').map((l) => {
     const [start, end] = l.split('~').map(parseLine);
     return {
       start,
@@ -26,12 +26,10 @@ function parser(input: string): Input {
   });
 }
 
-function cells(block: Block): Coordinate[] {
-  const [x1, y1, z1] = block.start;
-  const [x2, y2, z2] = block.end;
-  return range(x1, x2 + 1)
+function cells({ start: [x, y, z], end: [x2, y2, z2] }: Block): Coordinate[] {
+  return range(x, x2 + 1)
     .map((x) =>
-      range(y1, y2 + 1).map((y) => range(z1, z2 + 1).map((z) => [x, y, z])),
+      range(y, y2 + 1).map((y) => range(z, z2 + 1).map((z) => [x, y, z])),
     )
     .flat(2) as Coordinate[];
 }
@@ -57,14 +55,12 @@ function drop(block: Block, map: MaxZMap) {
       previousBest.z = z - drop;
     }
   });
-  return drop;
 }
 
 function dropBlocks(input: Input) {
   const maxZ = range(10).map(() => range(10).map(() => ({ z: 0 })));
-
   const todo = sortBy(input, (b) => max([b.start[2], b.end[2]]));
-  return sum(todo.map((block) => (drop(block, maxZ) ? 1 : 0)));
+  todo.forEach((block) => drop(block, maxZ));
 }
 
 function part1(input: Input) {
