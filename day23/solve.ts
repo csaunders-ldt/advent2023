d729cbb84301246f9a983d52dc42c7f6e31e2ee1
Chris Saunders <cjasaunders@outlook.com>
2023-12-23 23:53:48 +0000

    feat[day23]: initial cleanup

--- day23/solve.ts
+++ day23/solve.ts
@@ -1,5 +1,5 @@
-import { entries, keys, max, range } from 'lodash';
-import { solve, toVisualisation, visualisePoints } from '../utils/typescript';
+import { max, range } from 'lodash';
+import { solve } from '../utils/typescript';
 
 type Coordinate = [x: number, y: number];
 type Input = string[][];
@@ -16,103 +16,75 @@ function move([x, y]: Coordinate, dir: number, n = 1): Coordinate {
   return [x + ((2 - dir) % 2) * n, y + ((dir - 1) % 2) * n];
 }
 
-function isJunction(pos: Coordinate, grid: Input) {
-  const neighbours = range(4).map((dir) => move(pos, dir));
-  return neighbours.filter((p) => slides.includes(cell(p, grid))).length > 1;
+function eq([x1, y1]: Coordinate, [x2, y2]: Coordinate) {
+  return x1 === x2 && y1 === y2;
 }
 
 const slides = ['^', '>', 'v', '<'];
-function nextPos(state: Coordinate, grid: Input) {
-  const slide = slides.indexOf(cell(state, grid));
-  if (slide !== -1) {
-    return [move(state, slide)];
-  }
-  const opts = range(4).map((dir) => move(state, dir));
-  return opts.filter(
-    (pos, dir) =>
-      cell(pos, grid) === '.' || slides.indexOf(cell(pos, grid)) === dir,
-  );
+function isJunction(pos: Coordinate, grid: Input) {
+  const neighbours = range(4).map((dir) => move(pos, dir));
+  // TODO: Just count non-hash
+  return neighbours.filter((p) => slides.includes(cell(p, grid))).length > 1;
 }
 
-function next(
-  { pos, lastJunction, length }: Junction,
-  grid: Input,
-  routeMap: RouteMap,
-  seen: Set<string>,
-): Junction[] {
-  seen.add(String(pos));
-  const opts = nextPos(pos, grid).filter(
-    (p) => !seen.has(String(p)) || routeMap[String(p)],
-  );
-  if (isJunction(pos, grid) || opts.length === 0) {
-    const routes = (routeMap[String(lastJunction)] ??= {});
-    routes[String(pos)] = length + 1;
-    routeMap[String(pos)] ??= {};
-    return opts.map((opt) => ({ pos: opt, lastJunction: pos, length: 0 }));
+type Node = { idx: bigint; next: { dist: number; node: Node }[] };
+function dfs(node: Node, seen: bigint, target: bigint) {
+  const neighbours = node.next.filter(({ node }) => !(node.idx & seen));
+  if (!neighbours.length) {
+    return node.idx === target ? 0 : -9999;
   }
-  if (!opts.length) {
-    return [];
-  }
-  return [{ pos: opts[0], lastJunction, length: length + 1 }];
-}
-
-type Node = { idx: bigint; neighbours: { dist: number; node: Node }[] };
-function recursiveDfs(node: Node, seen: bigint) {
-  const neighbours = node.neighbours.filter(({ node }) => !(node.idx & seen));
-  if (!neighbours.length) return 0;
 
   return max(
-    neighbours.map((n) => n.dist + recursiveDfs(n.node, seen | node.idx)),
+    neighbours.map((n) => n.dist + dfs(n.node, seen | node.idx, target)),
   );
 }
 
-function dfsAgain(junctions: RouteMap) {
-  const entryKeys = keys(junctions);
-  const recursable = Object.entries(junctions).map(([k, v], i) => {
-    return { idx: BigInt(2 ** i), neighbours: [] };
-  });
-  Object.entries(junctions).forEach(([_, vals], i) => {
-    Object.entries(vals).forEach(([k, v]) => {
-      recursable[i].neighbours.push({
-        dist: v,
-        node: recursable[entryKeys.indexOf(k)],
-      });
-    });
-  });
-  return recursiveDfs(recursable[0] as Node, 0n) - 1;
-}
-
-type Junction = { pos: Coordinate; lastJunction: Coordinate; length: number };
-type RouteMap = Record<string, Record<string, number>>;
-
-function dfs(state: Junction[], grid: Input) {
-  let routes: RouteMap = {};
-  let seen = new Set<string>(state.map(({ pos }) => String(pos)));
-  while (state.length > 0) {
-    state.push(...next(state.shift(), grid, routes, seen));
-  }
-  return routes;
+function neighbours(cell: Coordinate) {
+  return range(4).map((dir) => move(cell, dir));
 }
 
 function part1(input: Input) {
-  const routes = dfs([{ pos: [1, 0], lastJunction: [0, 0], length: 0 }], input);
-  console.log(routes);
-  return dfsAgain(routes);
+  return -1;
+}
+
+function follow(curr: Coordinate, prev: Coordinate, grid: Input, len = 1) {
+  if (isJunction(curr, grid)) return { curr, len };
+  const next = neighbours(curr).find(
+    (n) => cell(n, grid) && cell(n, grid) !== '#' && !eq(prev, n),
+  );
+  if (!next) return { curr, len };
+  return follow(next, curr, grid, len + 1);
 }
 
 function part2(input: Input) {
-  const routes = dfs([{ pos: [1, 0], lastJunction: [0, 0], length: 0 }], input);
-  keys(routes)
-    .filter((k) => k !== '0,0')
-    .forEach((k) => {
-      entries(routes[k]).forEach(([pos, dist]) => {
-        if (routes[pos]) {
-          routes[pos][k] = dist;
-        }
-      });
-    });
-  console.log(routes);
-  return dfsAgain(routes);
+  const junctions = range(input.length).flatMap((y) =>
+    range(input[0].length)
+      .map((x) => [x, y] as Coordinate)
+      .filter(
+        ([x, y]) => cell([x, y], input) !== '#' && isJunction([x, y], input),
+      ),
+  );
+  junctions.unshift([1, 0]);
+  junctions.push([input.length - 2, input.length - 1]);
+  const nodes = junctions.map(
+    (_, i) =>
+      ({
+        idx: BigInt(2 ** i),
+        next: [],
+      }) as Node,
+  );
+  nodes.forEach((node, i) => {
+    const siblings = neighbours(junctions[i])
+      .filter((v) => cell(v, input) && cell(v, input) !== '#')
+      .map((loc) => follow(loc, junctions[i], input));
+    const nextNodes = siblings.map(({ curr, len }) => ({
+      node: nodes[junctions.findIndex((j) => eq(curr, j))],
+      dist: len,
+    }));
+    node.next.push(...nextNodes);
+  });
+
+  return dfs(nodes[0], 0n, nodes.at(-1).idx);
 }
 
 solve({ part1, test1: 94, part2, test2: 154, parser });
