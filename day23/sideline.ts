98a72134ac7a5f994dd96f2167c854837b6e9692
Chris Saunders <cjasaunders@outlook.com>
2023-12-23 22:00:50 +0000

    feat[day23]: prepping to break this thing

--- day23/sideline.ts
+++ day23/sideline.ts
@@ -17,7 +17,16 @@ function move([x, y]: Coordinate, dir: number, n = 1): Coordinate {
 }
 
 function isJunction(pos: Coordinate, grid: Input) {
+  if (cell(pos, grid) !== '.') return false; //REMOVEME
+
   const neighbours = range(4).map((dir) => move(pos, dir));
+  if (pos[0] === 103 && pos[1] == 125) {
+    console.log({
+      cells: neighbours.map((p) => cell(p, grid)),
+      val: neighbours.map((p) => slides.includes(cell(p, grid))),
+      res: neighbours.filter((p) => slides.includes(cell(p, grid))).length > 1,
+    });
+  }
   return neighbours.filter((p) => slides.includes(cell(p, grid))).length > 1;
 }
 
@@ -44,6 +53,15 @@ function next(
   const opts = nextPos(pos, grid).filter(
     (p) => !seen.has(String(p)) || routeMap[String(p)],
   );
+  if (!isJunction(pos, grid) && opts.length === 0) {
+    console.log({
+      pos,
+      vals: nextPos(pos, grid).map((p) => [
+        seen.has(String(p)),
+        routeMap[String(p)],
+      ]),
+    });
+  }
   if (isJunction(pos, grid) || opts.length === 0) {
     const routes = (routeMap[String(lastJunction)] ??= {});
     routes[String(pos)] = length + 1;
@@ -56,30 +74,24 @@ function next(
   return [{ pos: opts[0], lastJunction, length: length + 1 }];
 }
 
-type Node = { idx: bigint; neighbours: { dist: number; node: Node }[] };
-function recursiveDfs(node: Node, seen: bigint) {
-  const neighbours = node.neighbours.filter(({ node }) => !(node.idx & seen));
-  if (!neighbours.length) return 0;
-
-  return max(
-    neighbours.map((n) => n.dist + recursiveDfs(n.node, seen | node.idx)),
-  );
-}
-
 function dfsAgain(junctions: RouteMap) {
-  const entryKeys = keys(junctions);
-  const recursable = Object.entries(junctions).map(([k, v], i) => {
-    return { idx: BigInt(2 ** i), neighbours: [] };
-  });
-  Object.entries(junctions).forEach(([_, vals], i) => {
-    Object.entries(vals).forEach(([k, v]) => {
-      recursable[i].neighbours.push({
-        dist: v,
-        node: recursable[entryKeys.indexOf(k)],
-      });
-    });
-  });
-  return recursiveDfs(recursable[0] as Node, 0n) - 1;
+  let seen = new Set<string>();
+  let todo = [{ pos: keys(junctions)[0], score: 0 }];
+  let maxScore = 0;
+  while (todo.length > 0) {
+    const { pos, score } = todo.pop();
+
+    const links = entries(junctions[pos] ?? ({} as RouteMap[string]))
+      .filter(([p]) => !seen.has(p))
+      .map(([p, dist]) => ({
+        pos: p,
+        score: score + dist,
+        seen: new Set([...seen, p]),
+      }));
+    todo.push(...links);
+    maxScore = max([maxScore, ...todo.map(({ score }) => score)]);
+  }
+  return maxScore - 1;
 }
 
 type Junction = { pos: Coordinate; lastJunction: Coordinate; length: number };
@@ -96,11 +108,16 @@ function dfs(state: Junction[], grid: Input) {
 
 function part1(input: Input) {
   const routes = dfs([{ pos: [1, 0], lastJunction: [0, 0], length: 0 }], input);
-  console.log(routes);
   return dfsAgain(routes);
 }
 
 function part2(input: Input) {
+  const junctions = range(input.length).flatMap((y) =>
+    range(input[0].length)
+      .map((x) => [x, y])
+      .filter(([x, y]) => isJunction([x, y], input)),
+  );
+  console.log(junctions);
   const routes = dfs([{ pos: [1, 0], lastJunction: [0, 0], length: 0 }], input);
   keys(routes)
     .filter((k) => k !== '0,0')
