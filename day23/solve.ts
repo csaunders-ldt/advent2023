27738d07fdc02ff39fb511b42b4591be486c2e3e
Chris Saunders <cjasaunders@outlook.com>
2023-12-24 00:39:30 +0000

    refactor[day23]: part 1

--- day23/solve.ts
+++ day23/solve.ts
@@ -20,71 +20,70 @@ function eq([x1, y1]: Coordinate, [x2, y2]: Coordinate) {
   return x1 === x2 && y1 === y2;
 }
 
-const slides = ['^', '>', 'v', '<'];
 function isJunction(pos: Coordinate, grid: Input) {
+  if (cell(pos, grid) == '#') return false;
+
   const neighbours = range(4).map((dir) => move(pos, dir));
-  // TODO: Just count non-hash
-  return neighbours.filter((p) => slides.includes(cell(p, grid))).length > 1;
+  return neighbours.filter((p) => cell(p, grid) !== '#').length > 2;
 }
 
 type Node = { idx: bigint; next: { dist: number; node: Node }[] };
-function dfs(node: Node, seen: bigint, target: bigint) {
+function dfs(node: Node, seen: bigint, end: bigint) {
   const neighbours = node.next.filter(({ node }) => !(node.idx & seen));
-  if (!neighbours.length) {
-    return node.idx === target ? 0 : -9999;
-  }
+  if (!neighbours.length) return node.idx === end ? 0 : -9999;
 
-  return max(
-    neighbours.map((n) => n.dist + dfs(n.node, seen | node.idx, target)),
-  );
+  return max(neighbours.map((n) => n.dist + dfs(n.node, seen | node.idx, end)));
 }
 
-function neighbours(cell: Coordinate) {
-  return range(4).map((dir) => move(cell, dir));
+function neighbours(pos: Coordinate, grid: Input) {
+  const allNeighbours = range(4).map((dir) => move(pos, dir));
+  return allNeighbours.filter((v) => cell(v, grid) && cell(v, grid) !== '#');
 }
 
-function part1(input: Input) {
-  return -1;
+function follow(curr: Coordinate, prev: Coordinate, grid: Input, dist = 1) {
+  const next = neighbours(curr, grid).find((n) => !eq(prev, n));
+  if (isJunction(curr, grid) || !next) return { curr, dist };
+
+  return follow(next, curr, grid, dist + 1);
 }
 
-function follow(curr: Coordinate, prev: Coordinate, grid: Input, len = 1) {
-  if (isJunction(curr, grid)) return { curr, len };
-  const next = neighbours(curr).find(
-    (n) => cell(n, grid) && cell(n, grid) !== '#' && !eq(prev, n),
-  );
-  if (!next) return { curr, len };
-  return follow(next, curr, grid, len + 1);
+const cells = ['^', '>', 'v', '<', '.'];
+function rightDir(loc: Coordinate, grid: Input, dir: number) {
+  return dir === cells.indexOf(cell(loc, grid)) || cell(loc, grid) === '.';
 }
 
-function part2(input: Input) {
-  const junctions = range(input.length).flatMap((y) =>
-    range(input[0].length)
-      .map((x) => [x, y] as Coordinate)
-      .filter(
-        ([x, y]) => cell([x, y], input) !== '#' && isJunction([x, y], input),
-      ),
-  );
-  junctions.unshift([1, 0]);
-  junctions.push([input.length - 2, input.length - 1]);
-  const nodes = junctions.map(
-    (_, i) =>
-      ({
-        idx: BigInt(2 ** i),
-        next: [],
-      }) as Node,
-  );
+function findSiblings(pos: Coordinate, grid: Input, part2: boolean) {
+  const allSiblings = range(4).map((dir) => move(pos, dir));
+  const valid = allSiblings.filter((loc, i) => part2 || rightDir(loc, grid, i));
+  const notWall = valid.filter((v) => cells.includes(cell(v, grid)));
+  return notWall.map((loc) => follow(loc, pos, grid));
+}
+
+function getPathLength(grid: Input, part2 = false) {
+  const size = grid.length;
+  const cells = range(size).flatMap((y) => range(size).map((x) => [x, y]));
+  const junctions = cells.filter(([x, y]) => isJunction([x, y], grid));
+  junctions.unshift([1, 0], [grid.length - 2, grid.length - 1]);
+
+  const nodes = junctions.map((_, i) => ({ idx: BigInt(2 ** i), next: [] }));
   nodes.forEach((node, i) => {
-    const siblings = neighbours(junctions[i])
-      .filter((v) => cell(v, input) && cell(v, input) !== '#')
-      .map((loc) => follow(loc, junctions[i], input));
-    const nextNodes = siblings.map(({ curr, len }) => ({
-      node: nodes[junctions.findIndex((j) => eq(curr, j))],
-      dist: len,
-    }));
+    const pos = junctions[i] as Coordinate;
+    const nextNodes = findSiblings(pos, grid, part2).map(({ curr, dist }) => {
+      const node = nodes[junctions.findIndex((j) => eq(curr, j as Coordinate))];
+      return { node, dist };
+    });
     node.next.push(...nextNodes);
   });
 
-  return dfs(nodes[0], 0n, nodes.at(-1).idx);
+  return dfs(nodes[0], 0n, nodes[1].idx);
+}
+
+function part1(grid: Input) {
+  return getPathLength(grid);
+}
+
+function part2(grid: Input) {
+  return getPathLength(grid, true);
 }
 
 solve({ part1, test1: 94, part2, test2: 154, parser });
