7110c86f00db2c595f4d57278bd511d35a9c4f6b
Chris Saunders <cjasaunders@outlook.com>
2023-12-27 14:55:33 +0000

    refactor[day24]: slightly less ugly

--- day24/solve.ts
+++ day24/solve.ts
@@ -1,8 +1,7 @@
 import { chunk, range, sortBy, sum, zip } from 'lodash';
 import { solve } from '../utils/typescript';
-import { assert } from 'console';
 
-type Coordinate = [x: number, y: number, z: number];
+type Coordinate = [x: number, y: number];
 type Line = [
   [x: number, y: number, z: number],
   [dx: number, dy: number, dz: number],
@@ -25,115 +24,74 @@ function intersect([[x1, y1], [dx1, dy1]]: Line, [[x2, y2], [dx2, dy2]]: Line) {
   const y = slope1 * x + intercept1;
 
   const [t1, t2] = [(x - x1) / dx1, (x - x2) / dx2];
-  if (slope1 === slope2 || t1 < 0 || t2 < 0) {
-    return [Infinity, Infinity];
-  }
-
-  return [x, y];
+  const neverMeets = slope1 === slope2 || t1 < 0 || t2 < 0;
+  return (neverMeets ? [Infinity, Infinity] : [x, y]) as Coordinate;
 }
 
 function inArea([x, y]: Coordinate, [[x1, y1], [x2, y2]]: Coordinate[]) {
   return x >= x1 && x <= x2 && y >= y1 && y <= y2;
 }
 
-// Hailstone A: 19, 13, 30 @ -2, 1, -2
-// Hailstone B: 18, 19, 22 @ -1, -1, -2
-// Hailstones' paths will cross inside the test area (at x=14.333, y=15.333).
-
 const realArea: Coordinate[] = [
-  [2e14, 2e14, 2e14],
-  [4e14, 4e14, 4e14],
+  [2e14, 2e14],
+  [4e14, 4e14],
 ];
 const testArea: Coordinate[] = [
-  [7, 7, 7],
-  [27, 27, 27],
+  [7, 7],
+  [27, 27],
 ];
 function part1(input: Input, isTest: boolean) {
   const targetArea = isTest ? testArea : realArea;
-  const intersects = input.flatMap((line) => {
-    return input.map((other) => {
-      if (other === line) return 0;
-
-      const [x, y] = intersect(line, other);
-      return inArea([x, y, 0], targetArea) ? 1 : 0;
-    });
-  });
-  return sum(intersects) / 2;
+  const pairs = input.flatMap((line) => input.map((other) => [line, other]));
+  const intersects = pairs.filter(([line, other]) => {
+    return inArea(intersect(line, other), targetArea);
+  }).length;
+  return intersects / 2;
 }
 
 function gcd(a: number, b: number) {
   return b == 0 ? a : gcd(b, a % b);
 }
 
-function part2(input: Input, isTest: boolean) {
+function nextPrime(a: bigint, b: bigint, base = 1n, mod = 0n) {
+  if (a <= 1n) return base;
+  return nextPrime(b, a % b, mod, base - (a / b) * mod);
+}
+
+function part2(input: Input) {
   const [sumsOfPos, sumsOfDx] = zip(...input).map((v) => v.map(sum));
-  // TODO: This was 1000
   const res = range(-1000, 1000).map((answer) => {
-    if (sumsOfDx.includes(answer)) return false;
+    let basesAndMods = zip(sumsOfPos, sumsOfDx).map(([p, dx]) => [
+      Math.abs(dx - answer),
+      p % (dx - answer),
+    ]);
+    basesAndMods = sortBy(basesAndMods, ([m]) => -m);
 
-    let multipleAndRemainder = zip(sumsOfPos, sumsOfDx).map(([p, dx]) => {
-      let remainder = p % (dx - answer);
-      // This can probably be removed later
-      if (dx - answer < 0) remainder += dx - answer;
-      // 62 % (-3)
-      return [dx - answer, remainder] as const;
-    });
-    for (const i in range(multipleAndRemainder.length)) {
-      // TODO: JS Might just do this
-      const [multiple, remainder] = multipleAndRemainder[i];
-      if (multiple < 0) {
-        multipleAndRemainder[i] = [-multiple, remainder - multiple];
-      }
-    }
-    multipleAndRemainder = sortBy(multipleAndRemainder, ([m]) => -m);
-
-    // TODO: Bases, mods
     const bases: bigint[] = [];
-    const mods: number[] = [];
-    // remove coprimes
-    while (multipleAndRemainder.length > 0) {
-      const [base, mod] = multipleAndRemainder.shift();
+    const mods: bigint[] = [];
+    while (basesAndMods.length > 0) {
+      const [base, mod] = basesAndMods.shift();
       bases.push(BigInt(base));
-      mods.push(mod);
-      multipleAndRemainder = multipleAndRemainder.filter(
-        ([m]) => gcd(m, base) === 1,
-      );
+      mods.push(BigInt(mod));
+      basesAndMods = basesAndMods.filter(([m]) => gcd(m, base) === 1);
     }
 
-    // crt
-    let product = 1n;
-    for (const base of bases) {
-      product *= BigInt(base);
-    }
-    const inverses = bases.map((base) => product / BigInt(base));
+    const product = bases.reduce((acc, n) => acc * n, 1n);
+    const inverses = bases.map((base) => product / base);
 
-    const modifiedGcd = (a: bigint, b: bigint) => {
-      let initialB = b;
-      let [x0, x1] = [0n, 1n];
-      if (b === 1n) return 1n;
-      while (a > 1) {
-        let [div, mod] = [a / b, a % b];
-        [a, b] = [b, mod];
-        [x0, x1] = [x1 - div * x0, x0];
-      }
-      if (x1 >= 0) return x1;
-      return x1 + initialB;
-    };
-
-    const multiples = zip(bases, inverses).map(([base, inverse]) => {
-      return inverse * modifiedGcd(inverse, base);
-    });
-    let ret = 0n;
-    for (const [mul, mod] of zip(multiples, mods)) {
-      ret += BigInt(mul) * BigInt(mod);
-    }
-    const rem = ret % product;
+    const multiples = zip(bases, inverses).map(
+      ([base, inverse]) => inverse * nextPrime(inverse, base),
+    );
+    const sums = zip(multiples, mods).reduce(
+      (acc, [mul, mod]) => acc + mul * mod,
+      0n,
+    );
+    const remainder = sums % product;
     const positiveInt = (n: number) => n === Math.floor(n) && n > 0;
-    return zip(sumsOfPos, sumsOfDx).every(([pos, dx]) => {
-      return positiveInt((Number(rem) - pos) / (dx - answer));
-    })
-      ? rem
-      : 0;
+    const isDone = zip(sumsOfPos, sumsOfDx).every(([pos, dx]) => {
+      return positiveInt((Number(remainder) - pos) / (dx - answer));
+    });
+    return isDone ? remainder : 0;
   });
   return Number(res.find((v) => v));
 }
